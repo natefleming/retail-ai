f233566c86433a2451bd21aebb157e6c5c6a5df0
natefleming <natefleming>
2025-04-19 02:03:42 +0000

    Add custom spark datasource reader to load huggingface datasets

---
+++ retail_ai/catalog.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+
+from databricks.sdk import WorkspaceClient
+from databricks.sdk.service.catalog import CatalogInfo, SchemaInfo, VolumeInfo, VolumeType
+from databricks.sdk.errors.platform import NotFound
+
+
+def _volume_as_path(self: VolumeInfo) -> Path:
+  return Path(f"/Volumes/{self.catalog_name}/{self.schema_name}/{self.name}")
+
+# monkey patch
+VolumeInfo.as_path = _volume_as_path
+
+
+def get_or_create_catalog(name: str, w: WorkspaceClient | None = None) -> CatalogInfo:
+  if w is None:
+    w = WorkspaceClient()
+
+  catalog: CatalogInfo
+  try:
+    catalog = w.catalogs.get(name=name)
+  except NotFound as nf:
+    catalog = w.catalogs.create(name=name)
+  return catalog
+
+
+def get_or_create_database(catalog: CatalogInfo, name: str, w: WorkspaceClient | None = None) -> SchemaInfo:
+  if w is None:
+    w = WorkspaceClient()
+
+  database: SchemaInfo
+  try:
+    database = w.schemas.get(full_name=f"{catalog.name}.{name}")
+  except NotFound as nf:
+    database = w.schemas.create(name=name, catalog_name=catalog.name)
+  return database
+
+
+def get_or_create_volume(
+  database: SchemaInfo, 
+  name: str, 
+  volume_type: VolumeType = VolumeType.MANAGED,
+  w: WorkspaceClient | None = None
+) -> VolumeInfo:
+  if w is None:
+    w = WorkspaceClient()
+    
+  volume: VolumeInfo
+  try:
+    volume = w.volumes.read(name=f"{database.full_name}.{name}")
+  except NotFound as nf:
+    volume = w.volumes.create(
+      catalog_name=catalog.name, 
+      schema_name=database.name, 
+      name=name, 
+      volume_type=VolumeType.MANAGED
+    )
+  return volume
---
+++ retail_ai/vector_search.py
@@ -0,0 +1,22 @@
+
+from databricks.vector_search.client import VectorSearchClient
+
+def endpoint_exists(vsc: VectorSearchClient, vs_endpoint_name: str) -> bool:
+    try:
+        return vs_endpoint_name in [e['name'] for e in vsc.list_endpoints().get('endpoints', [])]
+    except Exception as e:
+        if "REQUEST_LIMIT_EXCEEDED" in str(e):
+            print("WARN: couldn't get endpoint status due to REQUEST_LIMIT_EXCEEDED error.")
+            return True
+        else:
+            raise e
+
+def index_exists(vsc: VectorSearchClient, endpoint_name: str, index_full_name: str) -> bool:
+    try:
+        vsc.get_index(endpoint_name, index_full_name).describe()
+        return True
+    except Exception as e:
+        if 'RESOURCE_DOES_NOT_EXIST' not in str(e):
+            print(f'Unexpected error describing the index. This could be a permission issue.')
+            raise e
+    return False
