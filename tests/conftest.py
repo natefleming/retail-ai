0509710a29da3862d4bba29193205c85c7c5d96c
Nate Fleming <nate.fleming@gmail.com>
2025-05-28 06:41:43 -1000

    move notebooks to dedicated directory

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,7 @@ from retail_ai.config import AppConfig
 root_dir: Path = Path(__file__).parents[1]
 src_dir: Path = root_dir / "retail_ai"
 test_dir: Path = root_dir / "tests"
+config_dir: Path = root_dir / "config"
 
 sys.path.insert(0, str(test_dir.resolve()))
 sys.path.insert(0, str(root_dir.resolve()))
@@ -16,14 +17,11 @@ sys.path.insert(0, str(root_dir.resolve()))
 
 @pytest.fixture
 def development_config() -> Path:
-    return root_dir / "model_config.yaml"
+    return config_dir / "model_config.yaml"
 
 
 @pytest.fixture
 def model_config(development_config: Path) -> ModelConfig:
-    """
-    Fixture to provide a sample ModelConfig for testing.
-    """
     return ModelConfig(development_config=development_config)
 
 
