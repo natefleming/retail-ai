5d165bb053ff0a50e625400d4fc1a99e69e31e6f
natefleming-db <157608612+natefleming-db@users.noreply.github.com>
2025-06-05 10:01:42 -0400

    add logic to infer postgres database type

--- retail_ai/config.py
+++ retail_ai/config.py
@@ -367,7 +367,7 @@ class StoreModel(BaseModel):
     def as_store(self) -> BaseStore:
         store: BaseStore = (
             self._as_postgres_store()
-            if self.type == StorageType.POSTGRES
+            if self.type == StorageType.POSTGRES or self.database and not self.embedding_model
             else self._as_in_memory_store()
         )
 
