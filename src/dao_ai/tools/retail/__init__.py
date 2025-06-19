0a056deb76fcd70bd2ccb7cc62aa65a7c91d906d
natefleming-db <157608612+natefleming-db@users.noreply.github.com>
2025-06-19 06:43:50 -0400

    move retail specific tools to submodule

---
+++ src/dao_ai/tools/retail/__init__.py
@@ -0,0 +1,19 @@
+from dao_ai.tools.retail.core import (
+    create_find_inventory_by_sku_tool,
+    create_find_inventory_by_upc_tool,
+    create_find_product_by_sku_tool,
+    create_find_product_by_upc_tool,
+    create_find_store_inventory_by_sku_tool,
+    create_find_store_inventory_by_upc_tool,
+    find_product_details_by_description_tool,
+)
+
+__all__ = [
+    "create_find_inventory_by_sku_tool",
+    "create_find_inventory_by_upc_tool",
+    "create_find_product_by_sku_tool",
+    "create_find_product_by_upc_tool",
+    "create_find_store_inventory_by_sku_tool",
+    "create_find_store_inventory_by_upc_tool",
+    "find_product_details_by_description_tool",
+]
